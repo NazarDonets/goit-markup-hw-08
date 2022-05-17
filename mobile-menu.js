31f6dbfd485be07ecd70af012162b44e659cb4f2
Nazar D <nazardonets@gmail.com>
2022-05-18 01:45:55 +0300

    disabled scroll when the mobile menu is open

--- mobile-menu.js
+++ mobile-menu.js
@@ -7,12 +7,10 @@
 		const expanded =
 			menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
 
-		menuBtnRef.classList.toggle('is-open');
 		menuBtnRef.setAttribute('aria-expanded', !expanded);
 
 		mobileMenuRef.classList.toggle('mobile-menu--is-open');
 		body.classList.toggle('body--scroll-hidden');
-
-		menuBtnRef.addEventListener('click', toggleScrollOff);
+		body.classList.toggle('body--scroll-hidden::-webkit-scrollbar');
 	});
 })();
